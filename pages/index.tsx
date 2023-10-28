f1782e4aef0b1197bdc7b1d6239af07e6df97755
Jesús Mata <andresmata.c21@gmail.com>
2023-10-28 14:33:36 -0600

    Render first 151 pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,46 @@
+import pokeAPI from '@/api/pokeAPI'
 import { Layout } from '@/components/layouts'
-import { Button } from '@nextui-org/react'
+import { PokeListResp, SmallPokemon } from '@/interfaces/pokeAPI'
+import { GetStaticProps, NextPage } from 'next'
 
-export default function HomePage() {
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Listado de Pokémons'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-
-      <Button color='primary'>Hola mundo</Button>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            # {pokemon.id} - {pokemon.name}
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeAPI.get<PokeListResp>('/pokemon?limit=151')
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => {
+    return {
+      ...pokemon,
+      id: i + 1,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+        i + 1
+      }.svg`,
+    }
+  })
+
+  // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg
+
+  return {
+    props: {
+      pokemons,
+    },
+  }
+}
+
+export default HomePage
